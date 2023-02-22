6e5c00dee56940486b1697d590b4ea75e0559388
이종원 <jklh0202@naver.com>
2023-02-22 12:41:19 +0900

    Time: 476 ms (34.01%), Space: 15.1 MB (100.00%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def shipWithinDays(self, weights, days):
-        L = max(weights)
-        R = 25000000
+        L = max(weights) #답이 될 수 있는 후보의 최소값
+        R = sum(weights) #답이 될 수 있는 후보의 최대값
         M = 0
+        
+        # 최대한 효율적으로 (컨베이어의 적재 무게를 최소화하는 Output)
 
         while(L <= R):
-            M = int((L + R) / 2)
+            M = int((L + R) / 2) # 지금 현재 '컨베이어' 의 무게.
             cnt = 1
-            sum = 0
+            sumt = 0
 
             for wei in weights:
-                if sum + wei <= M:
-                    sum += wei
+                if sumt + wei <= M: # 12500000
+                    sumt += wei
                 else :
                     cnt += 1
-                    sum = wei
+                    sumt = wei
 
             if cnt <= days : 
                 ans = M
