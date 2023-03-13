ea21802eaa67aa531b593afd8fd6683dab26b884
이종원 <jklh0202@naver.com>
2023-03-13 10:30:14 +0900

    Time: 39 ms (43.16%), Space: 14.2 MB (14.51%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        global que1, que2
+        que1 = []
+        que2 = []
+        
+        def LDFS(root, visited):
+            if root == None:
+                que1.append('null')
+                return
+            
+            visited.add(root)
+            que1.append(root.val)
+            
+            if not root.left in visited:
+                LDFS(root.left, visited)
+            if not root.right in visited:
+                LDFS(root.right, visited)
+            
+            
+        def RDFS(root, visited):
+            if root == None:
+                que2.append('null')
+                return
+            
+            visited.add(root)
+            que2.append(root.val)
+            if not root.right in visited:
+                RDFS(root.right, visited)
+            if not root.left in visited:
+                RDFS(root.left, visited)
+        
+        visited = set()
+        # que.clear()
+        LDFS(root, visited)
+        # LDFSQue = que
+        print(que1)
+        
+        # que.clear()
+        visited.clear()
+        rightList = RDFS(root, visited)
+        # RDFSQue = que
+        print(que2)
+        
+        return que1 == que2
+            
