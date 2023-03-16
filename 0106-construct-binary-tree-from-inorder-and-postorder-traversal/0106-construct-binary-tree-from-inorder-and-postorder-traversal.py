efa9acdfa4df4053781a4fd36b2f295bd47be4fd
이종원 <jklh0202@naver.com>
2023-03-16 11:31:46 +0900

    Update 0106-construct-binary-tree-from-inorder-and-postorder-traversal.py

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -7,19 +7,14 @@
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         def divideLR(inorder, postorder):
-            # postorder의 마지막 값이 항상 루트다. answer에 삽입하고,
-            # inorder에서 인덱스를 찾고, Lsubtree(5), Rsubtree(4) 개수를 파악해서, 
-            # inorder에서는 [0:rootIdx], [rootIdx+1:]
-            # postorder에서 [0:Lsubtree개수]를 postorder [5:-1]를 postorder. 항상 2개씩
-            # 그 후, 재귀탐색한다.
             if len(postorder) == 0:
                 return None
             if len(postorder) == 1:
                 return TreeNode(postorder[0])
-
-
+            
             root = TreeNode(postorder[-1])
             rootIdx = inorder.index(postorder[-1])
+            
             leftInorder = inorder[0:rootIdx]
             rightInorder = inorder[rootIdx+1:]
             leftPostorder = postorder[0:rootIdx]
